7ae302142a89cd1e0120435003d156bb00d3729d
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-12-15 21:05:56 -0500

    agregando metodos en el RestService

--- src/main/java/ec/ups/sistemas/apirest/controllers/ClienteRest.java
+++ src/main/java/ec/ups/sistemas/apirest/controllers/ClienteRest.java
@@ -3,9 +3,14 @@ package ec.ups.sistemas.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import ec.ups.sistemas.apirest.models.entity.Cliente;
@@ -22,6 +27,18 @@ public class ClienteRest {
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		return cs.findAll();
+	}
+	
+	@GetMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Cliente buscarCliente(@PathVariable Long id) {
+		return cs.findById(id);
+	}
+	
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente crear(@RequestBody Cliente cliente) {
+		return cs.save(cliente);
 	}
 	
 	
