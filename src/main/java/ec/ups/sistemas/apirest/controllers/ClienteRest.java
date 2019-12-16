10cf7136460812d4f0da4438942494ca1adc26ec
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-12-15 21:12:36 -0500

    agregando metodos actualizar y eliminar en el RestService

--- src/main/java/ec/ups/sistemas/apirest/controllers/ClienteRest.java
+++ src/main/java/ec/ups/sistemas/apirest/controllers/ClienteRest.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -39,6 +41,22 @@ public class ClienteRest {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente crear(@RequestBody Cliente cliente) {
 		return cs.save(cliente);
+	}
+	
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente actualizar(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente ct = cs.findById(id);
+		ct.setNombres(cliente.getNombres());
+		ct.setApellidos(cliente.getApellidos());
+		ct.setEmail(cliente.getEmail());
+		return cs.save(ct);
+	}
+	
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable long id) {
+		cs.delete(id);
 	}
 	
 	
